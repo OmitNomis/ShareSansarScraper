962c255b63fc3d3e047be6a68f3b6154f46e4290
OmitNomis <simon98190@gmail.com>
2024-06-17 15:23:58 +0545

    disable zip csv button till download starts

--- docs/js/script.js
+++ docs/js/script.js
@@ -36,28 +36,36 @@ function toggleCSVList() {
   }
 }
 
-fetch("Data/list_of_csv_files.txt")
-  .then((response) => response.text())
-  .then((csvList) => {
-    const csvFiles = csvList.trim().split("\n");
-    const csvFilesList = document.getElementById("csv-files-list");
-    csvFiles.forEach((csvFile) => {
-      const listItem = document.createElement("li");
-      const link = document.createElement("a");
-      link.href = `Data/${csvFile.trim()}`;
-      link.textContent = csvFile.trim();
-      link.download = csvFile.trim();
-      const button = document.createElement("button");
-      button.textContent = "Download";
-      button.onclick = () => {
-        const a = document.createElement("a");
-        a.href = link.href;
-        a.download = link.download;
-        a.click();
-      };
-      listItem.appendChild(link);
-      listItem.appendChild(button);
-      csvFilesList.appendChild(listItem);
+function downloadAllCSVs() {
+  const downloadButton = document.getElementById("download-csvs-btn");
+  downloadButton.disabled = true;
+
+  fetch("Data/list_of_csv_files.txt")
+    .then((response) => response.text())
+    .then((csvList) => {
+      const csvFiles = csvList.trim().split("\n");
+      const zip = new JSZip();
+
+      csvFiles.forEach((csvFile) => {
+        zip.file(
+          csvFile.trim(),
+          fetch(`Data/${csvFile.trim()}`).then((response) => response.blob()),
+        );
+      });
+
+      zip
+        .generateAsync({ type: "blob" })
+        .then((content) => {
+          saveAs(content, "all_csv_files.zip");
+          downloadButton.disabled = false;
+        })
+        .catch((error) => {
+          console.error("Error generating ZIP file:", error);
+          downloadButton.disabled = false;
+        });
+    })
+    .catch((error) => {
+      console.error("Error fetching CSVs list:", error);
+      downloadButton.disabled = false;
     });
-  })
-  .catch((error) => console.error("Error fetching CSV files list:", error));
+}
